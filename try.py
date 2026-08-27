472cfc8a5908cd59769c6bd12ece3ee3a8ed4549
JE Leabres <jleabres.k12257391@umak.edu.ph>
2026-08-27 20:22:04 +0800

    Chapter 3 seat work Conditional Execution

---
+++ try.py
@@ -0,0 +1,19 @@
+try:
+    total_score = input('Enter Total score: ')
+    score = float(total_score)
+    if score <= 90:
+        print('Grade: A')
+    elif score <= 80:
+        print('Grade: B')
+    elif score <= 70:
+        print('Grade: C')
+    elif score <= 69:
+        print('Grade: Needs improvement')
+    exit()
+
+except ValueError:
+    print('Error: Please enter a valid numerical score')
+    score = -1
+    exit()
+
+
